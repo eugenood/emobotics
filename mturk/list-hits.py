bfb5d2c4c1a9947f301501afc6268858cdbebecb
Eugene Lim <nexolute@gmail.com>
2020-11-30 15:08:10 +0800

    Fix list-hit bug

--- mturk/list-hits.py
+++ mturk/list-hits.py
@@ -2,6 +2,8 @@ import boto3
 import config
 import datetime
 
+endpoint_url = {'Sandbox': 'https://mturk-requester-sandbox.us-east-1.amazonaws.com', 'Production': 'https://mturk-requester.us-east-1.amazonaws.com'}
+
 client = boto3.client('mturk',
     aws_access_key_id=config.aws_access_key_id,
     aws_secret_access_key=config.aws_secret_access_key,
